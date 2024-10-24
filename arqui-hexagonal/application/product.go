6f3b6be30df125b72ca6cd4123da8d629d4e5898
José Augusto <josemoura212@gmail.com>
2024-10-24 20:58:32 -0300

    Refactorizar validação de produtos e adicionar testes unitários

--- arqui-hexagonal/application/product.go
+++ arqui-hexagonal/application/product.go
@@ -4,22 +4,43 @@ import (
 	"errors"
 
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
 
 
-func init	(){
+func init(){
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 type ProductInterface interface {
 	IsValid() (bool, error)
 	Enable() error
 	Disable() error
-
 	GetId() string
 	GetName() string
 	GetStatus() string
 	GetPrice() float64
 }
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
+
 
 const (
 	DISABLED = "disabled"
@@ -31,6 +52,15 @@ type Product struct {
 	Name   string `valid:"required"`
 	Price  float64 `valid:"float,optional"`
 	Status string `valid:"required"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		ID: uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+
+	return &product
 }
 
 func (p *Product) IsValid() (bool, error) {
