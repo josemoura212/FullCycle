add2b6b7d9911b31d7508d1aced54832905f6a06
José Augusto <josemoura212@gmail.com>
2024-10-25 21:22:27 -0300

    Refactorizar validação de produtos e adicionar testes unitários

--- arqui-hexagonal/main.go
+++ arqui-hexagonal/main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/josemoura212/FullCycle/arqui-hexagonal/adapters/db"
-	app "github.com/josemoura212/FullCycle/arqui-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/josemoura212/FullCycle/arqui-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db2.NewProductDb(db)
-	produectService := app.NewProductService(productDbAdapter)
-	product, _ := produectService.Create("Product Example", 30)
-
-	produectService.Enable(product)
+	cmd.Execute()
 }
