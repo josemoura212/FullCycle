386ba459cfbf6c8b51c845c38254de9b460b9770
José Augusto <josemoura212@gmail.com>
2024-11-29 14:06:30 -0300

    Adicionar configuração do servidor e rotas para gerenciamento de clientes com testes de integração

--- clean-architecture/src/usecase/custumer/find/find.customer.unit.spec.ts
+++ clean-architecture/src/usecase/custumer/find/find.customer.unit.spec.ts
@@ -1,10 +1,6 @@
-import { Sequelize } from "sequelize-typescript";
-import CustomerModel from "../../../infrastructure/customer/repository/sequelize/customer.model";
-import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
 import Customer from "../../../domain/customer/entity/customer";
 import Address from "../../../domain/customer/value-object/address";
 import FindCustomerUseCase from "./find.customer.usecase";
-import { create } from "yup/lib/Reference";
 
 const customer = new Customer("123", "John");
 const address = new Address("Street", 123, "Zip", "City 1");
