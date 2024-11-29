da8b6a49b754c218c1d46ceec8c7132b390b4a80
José Augusto <josemoura212@gmail.com>
2024-11-29 10:39:46 -0300

    Adicionar caso de uso e testes para listagem de produtos

---
+++ clean-architecture/src/usecase/product/list/list.product.usecase.ts
@@ -0,0 +1,29 @@
+import Product from "../../../domain/product/entity/product";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import { OutputListProductDto } from "./list.product.dto";
+
+export default class ListProductUseCase {
+  private productRepository: ProductRepositoryInterface;
+  constructor(productRepository: ProductRepositoryInterface) {
+    this.productRepository = productRepository;
+  }
+
+  async execute(): Promise<any> {
+    const products = await this.productRepository.findAll();
+    return OutputMapper.toOutput(products);
+  }
+}
+
+class OutputMapper {
+  static toOutput(products: Product[]): OutputListProductDto {
+    return {
+      products: products.map((p) => {
+        return {
+          id: p.id,
+          name: p.name,
+          price: p.price,
+        };
+      }),
+    };
+  }
+}
