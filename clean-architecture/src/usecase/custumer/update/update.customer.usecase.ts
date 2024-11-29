e8deee0ff09f2bd4b372c190d9ac5def74b8698b
José Augusto <josemoura212@gmail.com>
2024-11-28 21:15:54 -0300

    Adicionar caso de uso para atualização de cliente com implementação de lógica e repositório

--- clean-architecture/src/usecase/custumer/update/update.customer.usecase.ts
+++ clean-architecture/src/usecase/custumer/update/update.customer.usecase.ts
@@ -1,11 +1,40 @@
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
+import Address from "../../../domain/customer/value-object/address";
 import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
 import { OutputCreateCustomerDto } from "../create/create.customer.dto";
 import { InputUpdateCustomerDto } from "./update.customer.dto";
 
 export default class UpdateCustomerUseCase {
-  constructor(private customerRepository: CustomerRepository) {}
+  private customerRepository: CustomerRepositoryInterface;
+  constructor(customerRepository: CustomerRepositoryInterface) {
+    this.customerRepository = customerRepository;
+  }
 
   async execute(
     input: InputUpdateCustomerDto
-  ): Promise<OutputCreateCustomerDto> {}
+  ): Promise<OutputCreateCustomerDto> {
+    const customer = await this.customerRepository.find(input.id);
+    customer.changeName(input.name);
+    customer.changeAddress(
+      new Address(
+        input.address.street,
+        input.address.number,
+        input.address.zip,
+        input.address.city
+      )
+    );
+
+    await this.customerRepository.update(customer);
+
+    return {
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.Address.street,
+        number: customer.Address.number,
+        zip: customer.Address.zip,
+        city: customer.Address.city,
+      },
+    };
+  }
 }
