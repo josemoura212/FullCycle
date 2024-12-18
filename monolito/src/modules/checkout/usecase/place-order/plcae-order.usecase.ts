3ee0ba2cdc9ebf29dc087f5d17d75aa99b75182d
José Augusto <josemoura212@gmail.com>
2024-12-17 21:00:24 -0300

    Adicionar caso de uso para realizar pedidos; implementar validação de produtos e tratamento de erros para cliente e estoque

--- monolito/src/modules/checkout/usecase/place-order/plcae-order.usecase.ts
+++ monolito/src/modules/checkout/usecase/place-order/plcae-order.usecase.ts
@@ -1,9 +1,47 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
+import ClientAdmFacadaInterface from "../../../client-adm/facade/client-adm.facade.interface";
+import ProductAdmFacadeInterface from "../../../produtc-adm/facade/product-adm.facade.interface";
+import StoreCatalogFacadeInterface from "../../../store-catalog/facade/store-catalog.facade.interface";
+import Product from "../../domain/product.entity";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
-  constructor() {}
+  private _clientFacade: ClientAdmFacadaInterface;
+  private _productFacade: ProductAdmFacadeInterface;
+  private _catalogFacade: StoreCatalogFacadeInterface;
+
+  constructor(
+    clientFacade: ClientAdmFacadaInterface,
+    productFacade: ProductAdmFacadeInterface,
+    catalogFacade: StoreCatalogFacadeInterface
+  ) {
+    this._clientFacade = clientFacade;
+    this._productFacade = productFacade;
+    this._catalogFacade = catalogFacade;
+  }
+
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
+    //* buscar o cliente. caso nao encontrar -> client not found
+    const client = await this._clientFacade.find({ id: input.clientId });
+    if (!client) {
+      throw new Error("Client not found");
+    }
+
+    //* valida produto // funcao a parte
+    await this.validateProducts(input);
+
+    //* recuperar produtos
+
+    // criar o objeto do client
+    // criaro objeto do order (client,products)
+
+    // processpayment->paymentfacade.process (orderId,amount)
+
+    // caso pagamento seja aprovado. -> Gerar invoice
+    // mudar o status do order para approved
+    // retorna dto
+
     return {
       id: "1",
       invoiceId: "1",
@@ -11,5 +49,35 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
       total: 100,
       products: [{ productId: "1" }],
     };
+  }
+
+  private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
+    if (input.products.length === 0) {
+      throw new Error("No products selected");
+    }
+
+    for (const p of input.products) {
+      const product = await this._productFacade.checkStock({
+        productId: p.productId,
+      });
+      if (product.stock <= 0) {
+        throw new Error(
+          `Product ${product.productId} is not available in stock`
+        );
+      }
+    }
+  }
+
+  private async getProduct(productId: string): Promise<Product> {
+    const product = await this._catalogFacade.find({ id: productId });
+    if (!product) {
+      throw new Error("Product not found");
+    }
+    return new Product({
+      id: new Id(product.id),
+      name: product.name,
+      description: product.description,
+      salesPrice: product.salesPrice,
+    });
   }
 }
