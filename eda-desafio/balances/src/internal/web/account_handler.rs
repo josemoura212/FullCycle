4319642a09226db613d6cdd04536d6db1e7bd61b
José Augusto <josemoura212@gmail.com>
2025-04-17 22:24:41 -0300

    feat: atualizar dependências e refatorar manipulador de contas para melhorar a resposta HTTP

--- eda-desafio/balances/src/internal/web/account_handler.rs
+++ eda-desafio/balances/src/internal/web/account_handler.rs
@@ -1,17 +1,17 @@
-use actix_web::{Responder, web};
+use actix_web::{HttpResponse, web};
 
-use crate::internal::{Account, AccountGateway, account_db::AccountDb};
+use crate::internal::{AccountGateway, account_db::AccountDb};
 
 pub async fn get_balance(
     account_db: web::Data<AccountDb>,
     account_id: web::Path<String>,
-) -> impl Responder {
+) -> HttpResponse {
     let account_id = account_id.into_inner();
 
     let result = account_db.find_by_id(&account_id).await;
 
     match result {
-        Ok(account) => web::Json(account),
-        Err(_) => actix_web::HttpResponse::NotFound().body("Account not found"),
+        Ok(account) => HttpResponse::Ok().json(account),
+        Err(_) => HttpResponse::NotFound().body("Account not found"),
     }
 }
