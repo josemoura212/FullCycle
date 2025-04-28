ad8019c8ef547230aa3252cc6b8e5f6206662e77
José Augusto <josemoura212@gmail.com>
2025-04-28 14:11:04 -0300

    Atualiza estrutura do projeto Codeflix e adiciona testes
    O arquivo `.gitignore` foi atualizado para incluir diretórios e arquivos do Visual Studio e Visual Studio Code. Um novo diagrama foi adicionado ao `microsservicos.puml`, descrevendo a arquitetura do projeto com componentes como `Frontend`, `Backend`, `Database`, e `Keycloak`.
    
    Foi criado o arquivo `Catalog.sln` para definir a estrutura da solução, além de `Catalog.Domain.csproj` para configurar o projeto com o SDK do .NET e o framework `net8.0`. A classe `Category` foi implementada em `Category.cs`, e testes unitários foram adicionados em `CategoryTest.cs` para validar seu comportamento. Também foi criado o projeto de testes `Catalog.UnitTests.csproj` com as dependências necessárias.

---
+++ codeflix/Catalog/src/Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Catalog.Domain.Entity;
+public class Category(string name, string description)
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; private set; } = name;
+    public string Description { get; private set; } = description;
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
+    public bool IsActive { get; private set; } = true;
+}
